27549c5e6bde95e469d6e3a48131f0466ff86e92
TWhitwell <109387578+TWhitwell@users.noreply.github.com>
2022-08-31 19:45:20 +0100

    Looper v2 with catcher

--- looper.py
+++ looper.py
@@ -1,11 +1,27 @@
 import schedule
 import time
-import downloadchecker
+import sys
+
 
 def func():
-    exec(open("downloadchecker.py").read())
+    file = open('failedcounter.txt', 'r')
+    n = int(file.read())
+    if n <= 6:
+        exec(open("downloadchecker.py").read())
+    else:
+        subject = 'Script has failed 6 times'
+        plain = 'To prevent spamming it has been disabled'
+        html = '<b> To prevent spamming it has been disabled </b>'
+        exec(open("emailer.py").read())
+        print("failed")
+        schedule.clear()
+        file = open('failedcounter.txt', 'w')
+        file.write('0')
+        file.close()
+        n = 1
+        exit()
 
 schedule.every(1).minutes.do(func)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
